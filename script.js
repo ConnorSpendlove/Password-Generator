2eba14a81e4be3046c9df2bed0f3684f78ae5885
Connor Spendlove <Connor.Spendlove44@gmail.com>
2024-01-10 22:57:30 -0700

    Added prompts to appear upon user click

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 // Assignment code here
 
 // Default pasword length.
-var characterLengh = 8;
+var endingLength = "";
 // All posible character choices are stored here.
-var character = [];
+var userChoice = [];
 // All possible characters catergorized for user selection.
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",];
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9",];
 var specialCharacters = ["!", "@", "#", "$", "%", "&", "*",];
+var isLowerCase
+var isUpperCase
+var includesNumbers
+var includesSpecialCharacters
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -23,12 +27,41 @@ function writePassword() {
 }
 
 function generatePassword () {
+  var endingLength = parseInt(window.prompt("Please select a password length between 8 and 128 characters."));
+  console.log (endingLength);
+  if (endingLength < 8 || endingLength > 128 || isNaN(endingLength)){
+    alert("Error: Password must be a number between 8 and 128 characters.");
+    return "Error"
+  }
+  if (isLowerCase = confirm("Do you want to include lowercase letters?")){
+    (userChoice = userChoice + lowerCase)
+    console.log (isLowerCase);
+  }
+  if (isUpperCase = confirm("Do you want to include uppercase letters?")){
+    (userChoice = userChoice + upperCase)
+    console.log (isUpperCase);
+  }
+  if (includesNumbers = confirm("Do you want to include numbers?")){
+    (userChoice = userChoice + numbers)
+    console.log (includesNumbers);
+  }
+   if (includesSpecialCharacters = confirm("Do you want to include special characters?")){
+    (userChoice = userChoice + specialCharacters)
+    console.log (includesSpecialCharacters);
+  }
 
+  for (var i = 0; i < endingLength; i++) {
+    var random
+
+  }
 }
+
+
 
 function userSelection () {
 
-  
 }
+ 
+  
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
