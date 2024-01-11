42da319d6aeec9f73d86625767cb1f021c6ed96c
Connor Spendlove <Connor.Spendlove44@gmail.com>
2024-01-11 00:41:40 -0700

    Final bugfix added code that erases previous password when generating a new one

--- script.js
+++ script.js
@@ -5,22 +5,20 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 }
 
-// Sets password default to blank
-password = ""
-
-// Default pasword length.
+// Default password length.
 var endingLength = "";
+
 // Arrays for every possible character that can be used in the password.
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", ];
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", ];
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ];
-var specialCharacters = ["!", "@", "#", "$", "%", "&", "*", ];
+var specialCharacters = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~", ];
 
 function generatePassword () {
+  
   // All posible character choices are stored here.
   var userChoice = [];
 
@@ -33,14 +31,15 @@ function generatePassword () {
   // parseInt converts the user input from a string to a number so an error is not displayed because of the NaN clause.
   var endingLength = parseInt(window.prompt("Please select a password length between 8 and 128 characters."));
   
-  // If number is < 8 or > 128 or not a number, error alter will display.
-  if (endingLength < 8 || endingLength > 128 || isNaN(endingLength)){
+    // If x < 8, x > 128 or if x is not a number, an error alter will display in the window and in the console.
+    if (endingLength < 8 || endingLength > 128 || isNaN(endingLength)){
     alert("Error: Password must be a number between 8 and 128 characters.");
     console.log("Invalid password length")
     return "";
-  }
-// If user inputs a valid character length, this message is logged
-  if (endingLength){
+    }
+  
+    // If user inputs a valid character length, this message is logged.
+    if (endingLength){
     console.log ( "This password will be " + endingLength + " characters long!");
     }
 
@@ -73,7 +72,9 @@ function generatePassword () {
       alert("You must include at least 1 character type.")
       return "";
     }
-  
+    
+    // This line fixes 2 bugs. #1 it makes sure that only the password is displayed and not the element type. #2 it erases the previously generated password when a new one is created.
+    var password = ""; 
     // While i is less than the length of the password, a random value from the userChoice array will be selected and added to the password.
     for (var i = 0; i < endingLength; i++) {
       var random = Math.floor(Math.random() * userChoice.length);
@@ -82,11 +83,5 @@ function generatePassword () {
    return password;  
   }
 
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-
-
-  
