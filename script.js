e6c0f67c7ad71e1b7fe34394cc06bfdf0c5afd8a
Connor Spendlove <Connor.Spendlove44@gmail.com>
2024-01-10 23:36:19 -0700

    Added for loop to select random value from user array

--- script.js
+++ script.js
@@ -1,18 +1,11 @@
 // Assignment code here
 
 // Default pasword length.
-var endingLength = "";
-// All posible character choices are stored here.
-var userChoice = [];
-// All possible characters catergorized for user selection.
-var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
-var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",];
-var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9",];
-var specialCharacters = ["!", "@", "#", "$", "%", "&", "*",];
-var isLowerCase
-var isUpperCase
-var includesNumbers
-var includesSpecialCharacters
+var endingLength = "8";
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", ];
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", ];
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ];
+var specialCharacters = ["!", "@", "#", "$", "%", "&", "*", ];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -27,41 +20,54 @@ function writePassword() {
 }
 
 function generatePassword () {
-  var endingLength = parseInt(window.prompt("Please select a password length between 8 and 128 characters."));
+// All posible character choices are stored here.
+var userChoice = [];
+var includeLowerCase
+var includeUpperCase
+var includeNumbers
+var includeSpecialCharacters
+// parseInt converts the user input from a string to a number so an error is not displayed because of the NaN clause.
+var endingLength = parseInt(window.prompt("Please select a password length between 8 and 128 characters."));
   console.log (endingLength);
+
+  // If number is < 8 or > 128 or not a number, error alter will display.
   if (endingLength < 8 || endingLength > 128 || isNaN(endingLength)){
     alert("Error: Password must be a number between 8 and 128 characters.");
-    return "Error"
+    return "";
   }
-  if (isLowerCase = confirm("Do you want to include lowercase letters?")){
-    (userChoice = userChoice + lowerCase)
-    console.log (isLowerCase);
+  // If user selects ok, the lowerCase array is added to the userChoice variable.
+  if (includeLowerCase = confirm("Do you want to include lowercase letters?")){
+    (userChoice = userChoice.concat(lowerCase))
   }
-  if (isUpperCase = confirm("Do you want to include uppercase letters?")){
-    (userChoice = userChoice + upperCase)
-    console.log (isUpperCase);
+  // If user selects ok, the upperCase array is added to the userChoice variable.
+  if (includeUpperCase = confirm("Do you want to include uppercase letters?")){
+    (userChoice = userChoice.concat(upperCase))
   }
-  if (includesNumbers = confirm("Do you want to include numbers?")){
-    (userChoice = userChoice + numbers)
-    console.log (includesNumbers);
+  // If user selects ok, the numbers array is added to the userChoice variable.
+  if (includeNumbers = confirm("Do you want to include numbers?")){
+    (userChoice = userChoice.concat(numbers))
   }
-   if (includesSpecialCharacters = confirm("Do you want to include special characters?")){
-    (userChoice = userChoice + specialCharacters)
-    console.log (includesSpecialCharacters);
+  // If user selects ok, the specialCharacters array is added to the userChoice variable.
+   if (includeSpecialCharacters = confirm("Do you want to include special characters?")){
+    (userChoice = userChoice.concat(specialCharacters))
+  }
+  // Alerts user that they must select a character type
+  if (!includeLowerCase && !includeUpperCase && !includeNumbers && !includeSpecialCharacters){
+    alert("You must include at least 1 character type.")
+    return "";
   }
 
+  // While i is less than the length of the password, a random value from the userChoice array will be selected and added to the password.
   for (var i = 0; i < endingLength; i++) {
-    var random
-
-  }
+    var random = Math.floor(Math.random() * userChoice.length);
+    password = password + userChoice[random];
+   }
+ return password;  
 }
 
 
-
-function userSelection () {
-
-}
- 
-  
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+  
